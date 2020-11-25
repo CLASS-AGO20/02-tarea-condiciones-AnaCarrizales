efe7f2157a6504880b784f4d807af832b2a7bb67
Ana Carrizales <acarrizales2@ucol.mx>
2020-11-24 18:47:00 -0600

    esPar() was created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 export default class App {
+
+    esPar(numero)
+    {
+        if(numero % 2 === 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
  
 }
 
